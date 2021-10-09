b226fc44be890a36538e11f5aa29862954396fc2
AArora10 <77709572+AArora10@users.noreply.github.com>
2021-10-08 22:15:39 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 video = "";
 status = "";
+objects = [];
 
 function preload(){
     video = createVideo('video.mp4');
@@ -17,6 +18,26 @@ function draw(){
 
     image(video , 0,0, 480 , 380);
 
+    if (status != ""){
+        object_detector.detect( video , gotResult);
+
+        for ( i = 0; i < objects.length; i++){
+
+            document.getElementById("h2").innerHTML = "status : objects detected";
+            document.getElementById("label").innerHTML = "number of objects detetcted :" + objects.legnth;
+
+            fill("pink");
+            confidence = floor(objects[i].confidence * 100);
+            text( objects[i].label + "  " + confidence + "%", objects[i].x + 15 , objects[i].y) + 15;
+            noFill();
+            stroke("black");
+            rect( objects[i].x , objects[i].y , objects[i].width , objects[i].height);
+
+
+
+        }
+    }
+
 }
 
 function start(){
@@ -32,4 +53,13 @@ function modelLoaded(){
     video.loop();
     video.speed(1);
     video.volume(0);
+}
+function gotResult( results, error){
+    if(error){
+        console.log (error);
+    }
+    else{
+        console.log (results);
+        objects = results;
+    }
 }
